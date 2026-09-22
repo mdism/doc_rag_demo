e1e47eac26f3c80acb76d91931d38e87a879cf8f
mdism <md.ismaaill@gmail.com>
2026-09-22 18:25:46 +0530

    v0.5

--- reset_data.sh
+++ reset_data.sh
@@ -1,6 +1,3 @@
-#!/bin/bash
-# Clears all ingested document chunks so you can re-demo ingestion from a
-# clean state. Does NOT touch containers, the schema, or the vector
-# extension -- just empties the table.
+
 docker exec -it rag-postgres psql -U postgres -d rag_db -c "TRUNCATE TABLE data_employee_handbook;"
 echo "Knowledge base cleared. Ready to re-ingest."
